493b269e0181dfb05f20c3b1f3fcc904c58791bc
Toby Moreno <chris.moreno.ctr@km.spaceforce.mil>
2026-08-08 16:17:27 -0700

    Remove administration route from public application
    Signed-off-by: Toby Moreno <chris.moreno.ctr@km.spaceforce.mil>

--- test/cypress/e2e/administration.spec.ts
+++ test/cypress/e2e/administration.spec.ts
@@ -6,19 +6,22 @@ describe('/#/administration', () => {
     })
   })
   describe('challenge "adminSection"', () => {
-    it('should be possible to access administration section with admin user', () => {
+    it('should not expose the administration section in the public application', () => {
       cy.visit('/#/administration')
-      cy.url().should('match', /\/administration/)
-      cy.wait(1000) // added for debugging the CI
-      cy.expectChallengeSolved({ challenge: 'Admin Section' })
+      cy.get('app-administration').should('not.exist')
     })
   })
 
   describe('challenge "fiveStarFeedback"', () => {
-    it('should be possible for any admin user to delete feedback', () => {
-      cy.visit('/#/administration')
-      cy.wait(1000)
-      cy.get('.mat-mdc-cell.mat-column-remove > button').first().click()
+    it('should be possible for any admin user to delete feedback through the API', () => {
+      cy.request('/api/Feedbacks').then((response) => {
+        const feedback = response.body.data.find((entry: { rating: number }) => entry.rating === 5)
+        cy.request({
+          method: 'DELETE',
+          url: `/api/Feedbacks/${feedback.id}`,
+          headers: { Authorization: `Bearer ${localStorage.getItem('token')}` }
+        })
+      })
       cy.expectChallengeSolved({ challenge: 'Five-Star Feedback' })
     })
   })
