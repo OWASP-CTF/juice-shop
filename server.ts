3d788eb1f035bc687110df51eb07c77d446c1cee
Gabriel Seet <gabeseet@gmail.com>
2026-08-08 15:28:26 -0700

    fix: bind password reset limits to the client
    Signed-off-by: Gabriel Seet <gabeseet@gmail.com>

--- server.ts
+++ server.ts
@@ -346,8 +346,7 @@ function configureApp (app: ReturnType<typeof express>, seq: typeof sequelize) {
   app.enable('trust proxy')
   app.use('/rest/user/reset-password', rateLimit({
     windowMs: 5 * 60 * 1000,
-    max: 100,
-    keyGenerator ({ headers, ip }: { headers: any, ip: any }) { return headers['X-Forwarded-For'] ?? ip } // vuln-code-snippet vuln-line resetPasswordMortyChallenge
+    max: 100
   }))
   // vuln-code-snippet end resetPasswordMortyChallenge
 
