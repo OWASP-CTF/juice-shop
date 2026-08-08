703289df2362f72c3c86e2da86f8685eb5406c21
kabinet <67548726+kabinet01@users.noreply.github.com>
2026-08-08 21:26:19 +0000

    fix: enforce address update ownership
    Signed-off-by: kabinet <67548726+kabinet01@users.noreply.github.com>

--- data/static/codefixes/changeProductChallenge_3_correct.ts
+++ data/static/codefixes/changeProductChallenge_3_correct.ts
@@ -72,7 +72,7 @@
 
   app.post('/api/Addresss', security.appendUserId())
   app.get('/api/Addresss', security.appendUserId(), utils.asyncHandler(address.getAddress()))
-  app.put('/api/Addresss/:id', security.appendUserId())
+  app.put('/api/Addresss/:id', security.appendUserId(), utils.asyncHandler(address.enforceAddressOwnership()))
   app.delete('/api/Addresss/:id', security.appendUserId(), utils.asyncHandler(address.delAddressById()))
   app.get('/api/Addresss/:id', security.appendUserId(), utils.asyncHandler(address.getAddressById()))
   app.get('/api/Deliverys', utils.asyncHandler(delivery.getDeliveryMethods()))
