4e91f1d77232bbb43cc754d444b2886b35a3287a
Riki Länsilahti <7629042+lansiri@users.noreply.github.com>
2026-08-09 05:01:47 +0300

    Recursively sanitize feedback comments
    Signed-off-by: Riki Länsilahti <7629042+lansiri@users.noreply.github.com>

--- test/api/feedback.test.ts
+++ test/api/feedback.test.ts
@@ -9,9 +9,7 @@ import request from 'supertest'
 import type { Express } from 'express'
 import { createTestApp } from './helpers/setup'
 import { login } from './helpers/auth'
-import { challenges } from '../../data/datacache'
 import * as security from '../../lib/insecurity'
-import * as utils from '../../lib/utils'
 
 let app: Express
 const authHeader = { Authorization: 'Bearer ' + security.authorize(), 'content-type': 'application/json' }
@@ -48,26 +46,25 @@ void describe('/api/Feedbacks', () => {
     assert.equal(res.body.data.comment, 'I am a harmless comment.')
   })
 
-  if (utils.isChallengeEnabled(challenges.persistedXssFeedbackChallenge)) {
-    void it('POST fails to sanitize masked XSS-attack by not applying sanitization recursively', async () => {
-      const captchaRes = await request(app)
-        .get('/rest/captcha')
-      assert.equal(captchaRes.status, 200)
-      assert.ok(captchaRes.headers['content-type']?.includes('application/json'))
+  void it('POST recursively sanitizes masked XSS attacks', async () => {
+    const captchaRes = await request(app)
+      .get('/rest/captcha')
+    assert.equal(captchaRes.status, 200)
+    assert.ok(captchaRes.headers['content-type']?.includes('application/json'))
 
-      const res = await request(app)
-        .post('/api/Feedbacks')
-        .set(jsonHeader)
-        .send({
-          comment: 'The sanitize-html module up to at least version 1.4.2 has this issue: <<script>Foo</script>iframe src="javascript:alert(`xss`)">',
-          rating: 1,
-          captchaId: captchaRes.body.captchaId,
-          captcha: captchaRes.body.answer
-        })
-      assert.equal(res.status, 201)
-      assert.equal(res.body.data.comment, 'The sanitize-html module up to at least version 1.4.2 has this issue: <iframe src="javascript:alert(`xss`)">')
-    })
-  }
+    const res = await request(app)
+      .post('/api/Feedbacks')
+      .set(jsonHeader)
+      .send({
+        comment: 'The sanitize-html module up to at least version 1.4.2 has this issue: <<script>Foo</script>iframe src="javascript:alert(`xss`)">',
+        rating: 1,
+        captchaId: captchaRes.body.captchaId,
+        captcha: captchaRes.body.answer
+      })
+    assert.equal(res.status, 201)
+    assert.ok(!res.body.data.comment.includes('<iframe'))
+    assert.ok(!res.body.data.comment.includes('javascript:'))
+  })
 
   void it('POST feedback in another users name as anonymous user', async () => {
     const captchaRes = await request(app)
