3ac196dcc43dfdf0f1b1be4da5dc4cb020973ff5
Riki Länsilahti <7629042+lansiri@users.noreply.github.com>
2026-08-09 07:03:07 +0300

    Remove public Web3 sandbox route
    Signed-off-by: Riki Länsilahti <7629042+lansiri@users.noreply.github.com>

--- frontend/src/app/app.routing.ts
+++ frontend/src/app/app.routing.ts
@@ -51,11 +51,6 @@ const loadWeb3WalletModule = async () => {
   return module.WalletWeb3Module
 }
 
-const loadWeb3SandboxModule = async () => {
-  const module = await import('./web3-sandbox/web3-sandbox.module')
-  return module.Web3SandboxModule
-}
-
 const loadCodingChallenge = async () => {
   const module = await import('./coding-challenge-page/coding-challenge-page.component')
   return module.CodingChallengePageComponent
@@ -236,10 +231,6 @@ const routes: Routes = [
     path: 'wallet-web3',
     loadChildren: async () => await loadWeb3WalletModule()
   },
-  { // vuln-code-snippet neutral-line web3SandboxChallenge
-    path: 'web3-sandbox', // vuln-code-snippet vuln-line web3SandboxChallenge
-    loadChildren: async () => await loadWeb3SandboxModule() // vuln-code-snippet neutral-line web3SandboxChallenge
-  }, // vuln-code-snippet neutral-line web3SandboxChallenge
   {
     path: 'chatbot',
     component: ChatbotComponent,
