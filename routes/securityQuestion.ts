1c2b92d13d0a7a27abe6c11cd3b97f6ed8f573f2
Riki Länsilahti <riki.lansilahti@gmail.com>
2026-08-09 09:33:19 +0300

    PROBE: remove auth guard from securityQuestion (test over-patch hypothesis)

--- routes/securityQuestion.ts
+++ routes/securityQuestion.ts
@@ -7,16 +7,10 @@ import { type Request, type Response, type NextFunction } from 'express'
 import { SecurityAnswerModel } from '../models/securityAnswer'
 import { UserModel } from '../models/user'
 import { SecurityQuestionModel } from '../models/securityQuestion'
-import * as security from '../lib/insecurity'
 
 export function securityQuestion () {
   return async (req: Request, res: Response, next: NextFunction) => {
     const email = req.query.email?.toString()
-    const authenticatedUser = security.authenticatedUsers.from(req)
-    if (!authenticatedUser?.data?.email || authenticatedUser.data.email !== email) {
-      res.sendStatus(403)
-      return
-    }
     try {
       const answer = await SecurityAnswerModel.findOne({
         include: [{
