f12dfc1b493f9b9229591f5a7be5146b1f84fee5
freituneir <210881690+freituneir@users.noreply.github.com>
2026-08-09 05:17:14 -0400

    [measurement, do not merge] revert juice group D (auth, session, crypto, redirect, access control)

--- routes/resetPassword.ts
+++ routes/resetPassword.ts
@@ -43,7 +43,7 @@ export function resetPassword () {
         if (user) {
           const updatedUser = await user.update({ password: newPassword })
           verifySecurityAnswerChallenges(updatedUser, answer)
-          res.json({ user: { id: updatedUser.id, email: updatedUser.email } })
+          res.json({ user: updatedUser })
         }
       } else {
         res.status(401).send(res.__('Wrong answer to security question.'))
