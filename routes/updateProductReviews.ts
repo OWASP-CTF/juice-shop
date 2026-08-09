2210c5bf0989d897c4566d53af1fc758643bd1f3
Toby Moreno <chris.moreno.ctr@km.spaceforce.mil>
2026-08-09 07:33:36 -0700

    Enforce product review ownership
    Signed-off-by: Toby Moreno <chris.moreno.ctr@km.spaceforce.mil>

--- routes/updateProductReviews.ts
+++ routes/updateProductReviews.ts
@@ -14,8 +14,11 @@ import * as db from '../data/mongodb'
 export function updateProductReviews () {
   return (req: Request, res: Response, next: NextFunction) => {
     const user = security.authenticatedUsers.from(req) // vuln-code-snippet vuln-line forgedReviewChallenge
+    if (!user?.data?.email) {
+      return res.status(401).json({ error: 'Unauthorized' })
+    }
     db.reviewsCollection.update( // vuln-code-snippet neutral-line forgedReviewChallenge
-      { _id: req.body.id }, // vuln-code-snippet vuln-line noSqlReviewsChallenge forgedReviewChallenge
+      { _id: req.body.id, author: user.data.email }, // vuln-code-snippet vuln-line noSqlReviewsChallenge forgedReviewChallenge
       { $set: { message: req.body.message } },
       { multi: true } // vuln-code-snippet vuln-line noSqlReviewsChallenge
     ).then(
