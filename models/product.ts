b942ba2a49103586380e90fd8e7b4b3638800f06
freituneir <210881690+freituneir@users.noreply.github.com>
2026-08-09 04:56:21 -0400

    [measurement, do not merge] revert juice group A

--- models/product.ts
+++ models/product.ts
@@ -43,13 +43,16 @@ const ProductModelInit = (sequelize: Sequelize) => {
       description: {
         type: DataTypes.STRING,
         set (description: string) {
-          description = security.sanitizeSecure(description)
-          challengeUtils.solveIf(challenges.restfulXssChallenge, () => {
-            return utils.contains(
-              description,
-              '<iframe src="javascript:alert(`xss`)">'
-            )
-          })
+          if (utils.isChallengeEnabled(challenges.restfulXssChallenge)) {
+            challengeUtils.solveIf(challenges.restfulXssChallenge, () => {
+              return utils.contains(
+                description,
+                '<iframe src="javascript:alert(`xss`)">'
+              )
+            })
+          } else {
+            description = security.sanitizeSecure(description)
+          }
           this.setDataValue('description', description)
         }
       },
--- routes/likeProductReviews.ts
+++ routes/likeProductReviews.ts
@@ -8,7 +8,10 @@ import { type Request, type Response, type NextFunction } from 'express'
 import * as challengeUtils from '../lib/challengeUtils'
 import { challenges } from '../data/datacache'
 import * as security from '../lib/insecurity'
+import { type Review } from '../data/types'
 import * as db from '../data/mongodb'
+
+const sleep = async (ms: number) => await new Promise(resolve => setTimeout(resolve, ms))
 
 export function likeProductReviews () {
   return async (req: Request, res: Response, next: NextFunction) => {
@@ -19,16 +22,39 @@ export function likeProductReviews () {
     }
 
     try {
-      const result = await db.reviewsCollection.update(
-        { _id: id, likedBy: { $ne: user.data.email } },
-        { $inc: { likesCount: 1 }, $addToSet: { likedBy: user.data.email } }
-      )
-      const modified = (result as any)?.nModified ?? (result as any)?.modified ?? 0
-      challengeUtils.solveIf(challenges.timingAttackChallenge, () => modified > 1)
-      if (!modified) {
+      const review = await db.reviewsCollection.findOne({ _id: id })
+      if (!review) {
+        return res.status(404).json({ error: 'Not found' })
+      }
+
+      const likedBy = review.likedBy
+      if (likedBy.includes(user.data.email)) {
         return res.status(403).json({ error: 'Not allowed' })
       }
-      res.json(result)
+
+      await db.reviewsCollection.update(
+        { _id: id },
+        { $inc: { likesCount: 1 } }
+      )
+
+      // Artificial wait for timing attack challenge
+      await sleep(150)
+      try {
+        const updatedReview: Review = await db.reviewsCollection.findOne({ _id: id })
+        const updatedLikedBy = updatedReview.likedBy
+        updatedLikedBy.push(user.data.email)
+
+        const count = updatedLikedBy.filter(email => email === user.data.email).length
+        challengeUtils.solveIf(challenges.timingAttackChallenge, () => count > 2)
+
+        const result = await db.reviewsCollection.update(
+          { _id: id },
+          { $set: { likedBy: updatedLikedBy } }
+        )
+        res.json(result)
+      } catch (err) {
+        res.status(500).json(err)
+      }
     } catch (err) {
       res.status(400).json({ error: 'Wrong Params' })
     }
