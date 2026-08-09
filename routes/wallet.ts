659ad31cc723438f4bc5666e440835a4df237ffd
Toby Moreno <chris.moreno.ctr@km.spaceforce.mil>
2026-08-09 07:16:32 -0700

    Enforce server-side ownership for wallets
    Signed-off-by: Toby Moreno <chris.moreno.ctr@km.spaceforce.mil>

--- routes/wallet.ts
+++ routes/wallet.ts
@@ -6,10 +6,13 @@
 import { type Request, type Response, type NextFunction } from 'express'
 import { WalletModel } from '../models/wallet'
 import { CardModel } from '../models/card'
+import * as security from '../lib/insecurity'
 
 export function getWalletBalance () {
   return async (req: Request, res: Response, next: NextFunction) => {
-    const wallet = await WalletModel.findOne({ where: { UserId: req.body.UserId } })
+    const user = security.authenticatedUsers.from(req)
+    if (!user?.data?.id) return res.status(401).json({ status: 'error' })
+    const wallet = await WalletModel.findOne({ where: { UserId: user.data.id } })
     if (wallet != null) {
       res.status(200).json({ status: 'success', data: wallet.balance })
     } else {
@@ -20,11 +23,13 @@ export function getWalletBalance () {
 
 export function addWalletBalance () {
   return async (req: Request, res: Response, next: NextFunction) => {
+    const user = security.authenticatedUsers.from(req)
+    if (!user?.data?.id) return res.status(401).json({ status: 'error' })
     const cardId = req.body.paymentId
-    const card = cardId ? await CardModel.findOne({ where: { id: cardId, UserId: req.body.UserId } }) : null
+    const card = cardId ? await CardModel.findOne({ where: { id: cardId, UserId: user.data.id } }) : null
     if (card != null) {
       try {
-        await WalletModel.increment({ balance: req.body.balance }, { where: { UserId: req.body.UserId } })
+        await WalletModel.increment({ balance: req.body.balance }, { where: { UserId: user.data.id } })
         res.status(200).json({ status: 'success', data: req.body.balance })
       } catch {
         res.status(404).json({ status: 'error' })
