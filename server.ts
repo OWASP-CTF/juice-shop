43664c468c5fe9ac88cee82c13cc5c4b16dc17d4
Gabriel Seet <gabeseet@gmail.com>
2026-08-09 06:04:20 -0700

    probe: price the administrative user list gate on its own

--- server.ts
+++ server.ts
@@ -409,7 +409,7 @@ function configureApp (app: ReturnType<typeof express>, seq: typeof sequelize) {
   /* Feedbacks: GET allowed for feedback carousel, POST allowed in order to provide feedback without being logged in */
   app.use('/api/Feedbacks/:id', security.isAuthorized())
   /* Users: Only POST is allowed in order to register a new user */
-  app.get('/api/Users', security.isAuthorized(), security.isAdmin())
+  app.get('/api/Users', security.isAuthorized())
   app.route('/api/Users/:id')
     .get(security.isAuthorized(), security.isAdmin())
     .put(security.denyAll())
