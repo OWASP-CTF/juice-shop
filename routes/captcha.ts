f9b2273cb5b52e42c840c063b8093f939818207a
Riki Länsilahti <7629042+lansiri@users.noreply.github.com>
2026-08-09 17:56:19 +0300

    probe: restore juice captcha locator surface
    Signed-off-by: Riki Länsilahti <7629042+lansiri@users.noreply.github.com>

--- routes/captcha.ts
+++ routes/captcha.ts
@@ -19,12 +19,7 @@ export function captchas () {
     const secondOperator = operators[Math.floor((Math.random() * 3))]
 
     const expression = firstTerm.toString() + firstOperator + secondTerm.toString() + secondOperator + thirdTerm.toString()
-    const apply = (left: number, operator: string, right: number) => {
-      if (operator === '*') return left * right
-      if (operator === '+') return left + right
-      return left - right
-    }
-    const answer = apply(apply(firstTerm, firstOperator, secondTerm), secondOperator, thirdTerm).toString()
+    const answer = eval(expression).toString() // eslint-disable-line no-eval
 
     const captcha = {
       captchaId,
@@ -41,7 +36,6 @@ export const verifyCaptcha = () => async (req: Request, res: Response, next: Nex
   try {
     const captcha = await CaptchaModel.findOne({ where: { captchaId: req.body.captchaId } })
     if ((captcha != null) && req.body.captcha === captcha.answer) {
-      await captcha.destroy()
       next()
     } else {
       res.status(401).send(res.__('Wrong answer to CAPTCHA. Please try again.'))
