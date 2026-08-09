6239d6633ee917c50fe5dd5499dabad18963323c
Riki Länsilahti <7629042+lansiri@users.noreply.github.com>
2026-08-09 07:47:06 +0300

    Revert "Sanitize public user registration input"
    This reverts commit e02cb32dcf436795aa8f0909572d3e18f2debb33.
    
    Signed-off-by: Riki Länsilahti <7629042+lansiri@users.noreply.github.com>

--- models/user.ts
+++ models/user.ts
@@ -13,6 +13,9 @@ import {
   type CreationOptional,
   type Sequelize
 } from 'sequelize'
+import * as challengeUtils from '../lib/challengeUtils'
+import * as utils from '../lib/utils'
+import { challenges } from '../data/datacache'
 import * as security from '../lib/insecurity'
 
 class User extends Model<
@@ -43,14 +46,29 @@ const UserModelInit = (sequelize: Sequelize) => { // vuln-code-snippet start wea
         type: DataTypes.STRING,
         defaultValue: '',
         set (username: string) {
-          this.setDataValue('username', security.sanitizeSecure(username))
+          if (utils.isChallengeEnabled(challenges.persistedXssUserChallenge)) {
+            username = security.sanitizeLegacy(username)
+          } else {
+            username = security.sanitizeSecure(username)
+          }
+          this.setDataValue('username', username)
         }
       },
       email: {
         type: DataTypes.STRING,
         unique: true,
         set (email: string) {
-          this.setDataValue('email', security.sanitizeSecure(email))
+          if (utils.isChallengeEnabled(challenges.persistedXssUserChallenge)) {
+            challengeUtils.solveIf(challenges.persistedXssUserChallenge, () => {
+              return utils.contains(
+                email,
+                '<iframe src="javascript:alert(`xss`)">'
+              )
+            })
+          } else {
+            email = security.sanitizeSecure(email)
+          }
+          this.setDataValue('email', email)
         }
       }, // vuln-code-snippet hide-end
       password: {
