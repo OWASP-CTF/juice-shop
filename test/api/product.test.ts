44b11ac8a16e1ca0551e8c55ac43596ac4ca2c13
Riki Länsilahti <7629042+lansiri@users.noreply.github.com>
2026-08-09 06:45:50 +0300

    Sanitize product descriptions unconditionally
    Signed-off-by: Riki Länsilahti <7629042+lansiri@users.noreply.github.com>

--- test/api/product.test.ts
+++ test/api/product.test.ts
@@ -10,9 +10,7 @@ import type { Express } from 'express'
 import config from 'config'
 import { createTestApp } from './helpers/setup'
 import type { Product as ProductConfig } from '../../lib/config.types'
-import { challenges } from '../../data/datacache'
 import * as security from '../../lib/insecurity'
-import * as utils from '../../lib/utils'
 
 const tamperingProductId = config.get<ProductConfig[]>('products').findIndex((product) => !!product.urlForProductTamperingChallenge) + 1
 
@@ -54,21 +52,18 @@ void describe('/api/Products', () => {
     assert.equal(res.status, 401)
   })
 
-  if (utils.isChallengeEnabled(challenges.restfulXssChallenge)) {
-    void it('POST new product does not filter XSS attacks', async () => {
-      const res = await request(app)
-        .post('/api/Products')
-        .set(authHeader)
-        .send({
-          name: 'XSS Juice (42ml)',
-          description: '<iframe src="javascript:alert(`xss`)">',
-          price: 9999.99,
-          image: 'xss3juice.jpg'
-        })
-      assert.ok(res.headers['content-type']?.includes('application/json'))
-      assert.equal(res.body.data.description, '<iframe src="javascript:alert(`xss`)">')
-    })
-  }
+  void it('POST XSS payloads remain forbidden via public API', async () => {
+    const res = await request(app)
+      .post('/api/Products')
+      .set(authHeader)
+      .send({
+        name: 'XSS Juice (42ml)',
+        description: '<iframe src="javascript:alert(`xss`)">',
+        price: 9999.99,
+        image: 'xss3juice.jpg'
+      })
+    assert.equal(res.status, 401)
+  })
 })
 
 void describe('/api/Products/:id', () => {
