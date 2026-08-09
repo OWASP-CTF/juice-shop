77975dc495dd76622ff1ed65cf6d74f15ba29b0b
Riki Länsilahti <7629042+lansiri@users.noreply.github.com>
2026-08-09 03:19:17 +0300

    Remove dynamic evaluation from request handling
    Signed-off-by: Riki Länsilahti <7629042+lansiri@users.noreply.github.com>

--- routes/captcha.ts
+++ routes/captcha.ts
@@ -19,7 +19,12 @@ export function captchas () {
     const secondOperator = operators[Math.floor((Math.random() * 3))]
 
     const expression = firstTerm.toString() + firstOperator + secondTerm.toString() + secondOperator + thirdTerm.toString()
-    const answer = eval(expression).toString() // eslint-disable-line no-eval
+    const apply = (left: number, operator: string, right: number) => {
+      if (operator === '*') return left * right
+      if (operator === '+') return left + right
+      return left - right
+    }
+    const answer = apply(apply(firstTerm, firstOperator, secondTerm), secondOperator, thirdTerm).toString()
 
     const captcha = {
       captchaId,
